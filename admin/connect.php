b307000372ec338506ffc397277f854b7352aa38
unknown <kusemererwaedson2000@gmail.com>
2022-10-13 10:46:04 +0300

    hey

--- admin/connect.php
+++ admin/connect.php
@@ -1,2 +1,2 @@
 <?php
-	$conn = new mysqli("localhost", "root", "", "db_hor") or die(mysqli_error());
+	$conn = new mysqli("localhost", "campusgf_edy", "Eddiespacks2000", "campusgf_room_booking") or die(mysqli_error());
