f02a74a434fd0f0e269792a390590afe24c4dd29
edsonkusemererwa <Kusemererwaedson2000@gmail.com>
2022-10-18 05:13:58 -0700

    connect.php

--- admin/connect.php
+++ admin/connect.php
@@ -1,6 +1,5 @@
 <?php
-	$conn = new mysqli("localhost", "root", "", "db_hor") or die(mysqli_error());
+	$conn = new mysqli("localhost", "campus_gf_edy", "Eddiespacks2000", "campus_gf_room_booking) or die(mysqli_error());
 
 
-	// campus_gf_edy
-	// campus_gf_edy
+	?>
